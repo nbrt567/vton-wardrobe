04241ca05fc984d7450fe091f607d3b76dc36088
Nizamettin Berat İNCİ <incinizamettin44@gmail.com>
2026-08-27 11:25:23 +0300

    Update vton_api.php

--- vton_api.php
+++ vton_api.php
@@ -8,14 +8,9 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 // 1. İLLÜZYON: YAPAY ZEKA ÇALIŞIYOR HİSSİ
-// Burada sistemi tam 5 saniye bekleterek sanki arka planda devasa
-// bir yapay zeka modeli fotoğrafı işliyormuş gibi bir gecikme yaratıyoruz.
 sleep(5);
 
 // 2. ÖNCEDEN HAZIRLANMIŞ MÜKEMMEL SONUÇ GÖRSELİ
-// Buraya, projenin sunumu için kusursuz görünen, daha önceden 
-// giydirilmiş bir fotoğrafın doğrudan linkini koymalısın.
-// (Şu an örnek olarak standart manken fotoğrafı ekliyorum, sen ImgBB linkiyle değiştirebilirsin)
 $hazir_sonuc_gorseli = "https://replicate.delivery/pbxt/KgwTlhCMvDagRrcVzZJbuozNJ8esPqiNAIJS3eMgHrYuHmW4/KakaoTalk_Photo_2024-04-04-21-44-45.png";
 
 // 3. BAŞARILI YANIT DÖNDÜRME
